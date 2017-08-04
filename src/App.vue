7489e8e94cbec0a8224423d93fe21cf07e4dbbd1
joshimraw <joshim7m@gmail.com>
2017-08-04 11:46:58 +0600

    This is an awesome quoting system

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="container">
+		<app-newquote @quoteAdded="addNewQuote"></app-newquote>
+		<hr>
+		<app-quotegrid :quotes="quotes" @quoteDeleted="quoteDelete"></app-quotegrid>
+	</div>
+</template>
+
+
+<script>
+	import Newquote from './Newquote.vue';
+	import Quotegrid from './Quotegrid.vue';
+	export default{
+		data: function(){
+			return {
+				quotes: [
+					{ body: 'Somethign to display', title: 'Title of quote' }
+
+				]
+			}
+		},
+		components: {
+			appQuotegrid: Quotegrid,
+			appNewquote: Newquote
+		},
+		methods: {
+			addNewQuote(quote){
+				this.quotes.push(quote);
+			},
+			quoteDelete(index){
+				this.quotes.splice(index, 1);
+			}
+		}
+	}
+</script>
---
+++ src/Newquote.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="row">
+		<form>
+			<div class="col-md-6 col-md-offset-3 form-group">
+				<label class="lead">Add New Quote</label>
+				<input type="text" class="form-control" placeholder="your title here" v-model="quotes.title">
+			</div>
+			<div class="col-md-6 col-md-offset-3 form-group">
+				<textarea class="form-control" rows="3" 
+				v-model="quotes.body" @keyup.enter="addNew" placeholder="write something here"> </textarea>
+			</div>
+
+			<div class="col-md-6 col-md-offset-3 form-group">
+			<button class="btn btn-primary pull-right" @click.prevent="addNew">Submit</button>
+			</div>
+		</form>
+	</div>
+</template>
+
+<script>
+	export default{
+		data: function(){
+			return {
+				quotes: {
+					body: '',
+					title: ''
+				}
+			}
+		},
+		methods: {
+			addNew(quote){
+				this.$emit('quoteAdded', this.quotes);
+				this.quotes = '';
+			}
+		}
+	}
+</script>
